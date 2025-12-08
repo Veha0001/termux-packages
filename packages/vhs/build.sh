3db7b951b3de42059ab0ea12bad26256a84b7102
Veha Veha <84841152+Veha0001@users.noreply.github.com>
2025-12-08 16:50:20 +0700

    chore(main/vhs): 0.10.0 fix chrome

--- packages/vhs/build.sh
+++ packages/vhs/build.sh
@@ -14,13 +14,25 @@ termux_step_configure(){
 }
 
 termux_step_make(){
-	sed -i 's|path, _ := launcher.LookPath()|path := "${TERMUX_PREFIX}/bin/chromium-browser"|' $TERMUX_PKG_SRCDIR/vhs.go
+	sed -i "s|path, _ := launcher.LookPath()|path := \"${TERMUX_PREFIX}/bin/chromium-browser\"|" "$TERMUX_PKG_SRCDIR/vhs.go"
 	go build -o vhs -ldflags "-compressdwarf=false \
 	-X main.Version=${TERMUX_PKG_VERSION} \
 	-X main.CommitSHA=$(git rev-parse HEAD) \
 	-X main.CommitDate=$(git show --no-patch --format=%cd --date=format:%Y-%m-%d)"
+	
 }
 
 termux_step_make_install(){
+	mkdir -p "${TERMUX_PREFIX}/share/man/man1"
+	mkdir -p "${TERMUX_PREFIX}/share/zsh/site-functions"
+	mkdir -p "${TERMUX_PREFIX}/share/fish/vendor_completions.d"
+	mkdir -p "${TERMUX_PREFIX}/share/bash-completion/completions"
+	
+	unset GOOS GOARCH CGO_LDFLAGS
+	unset CC CXX CFLAGS CXXFLAGS LDFLAGS
+	go run .             man > "${TERMUX_PREFIX}/share/man/man1/${TERMUX_PKG_NAME}.1"
+	go run . completion  zsh > "${TERMUX_PREFIX}/share/zsh/site-functions/_${TERMUX_PKG_NAME}"
+	go run . completion bash > "${TERMUX_PREFIX}/share/bash-completion/completions/${TERMUX_PKG_NAME}"
+	go run . completion fish > "${TERMUX_PREFIX}/share/fish/vendor_completions.d/${TERMUX_PKG_NAME}.fish"
 	install -Dm755 -t "${TERMUX_PREFIX}"/bin vhs
 }
