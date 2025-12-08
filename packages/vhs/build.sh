8d4dd1604eee8a11b68e9eced52fc95f0397857a
Veha Veha <84841152+Veha0001@users.noreply.github.com>
2025-12-08 16:21:57 +0700

    chore(main/vhs): git version control

--- packages/vhs/build.sh
+++ packages/vhs/build.sh
@@ -3,18 +3,22 @@ TERMUX_PKG_DESCRIPTION="Your CLI home video recorder"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="0.10.0"
-TERMUX_PKG_SRCURL=https://github.com/charmbracelet/vhs/releases/download/v${TERMUX_PKG_VERSION}/vhs-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d971df9d2ec365db116c3c770aee25bcfb87082fc19e808209766bb4f6561cab
+TERMUX_PKG_SRCURL=git+https://github.com/charmbracelet/vhs
+TERMUX_PKG_GIT_BRANCH="v${TERMUX_PKG_VERSION}"
 TERMUX_PKG_DEPENDS="ttyd"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 
 termux_step_configure(){
 	termux_setup_golang
 }
 
 termux_step_make(){
-	sed -i 's|path, _ := launcher.LookPath()|path := "${TERMUX_PREFIX}/bin/chromium-browser"|' vhs.go
-	go build -o vhs
+	sed -i 's|path, _ := launcher.LookPath()|path := "${TERMUX_PREFIX}/bin/chromium-browser"|' $TERMUX_PKG_SRCDIR/vhs.go
+	go build -o vhs -ldflags "-compressdwarf=false \
+	-X main.Version=${TERMUX_PKG_VERSION} \
+	-X main.CommitSHA=$(git rev-parse HEAD) \
+	-X main.CommitDate=$(git show --no-patch --format=%cd --date=format:%Y-%m-%d)"
 }
 
 termux_step_make_install(){
