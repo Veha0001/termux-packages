0bb8c5055f1b34c3e0f5645f395b426f16ebd2b8
Veha Veha <84841152+Veha0001@users.noreply.github.com>
2025-12-08 18:10:58 +0700

    Update build flags for vhs compilation

--- packages/vhs/build.sh
+++ packages/vhs/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_VERSION="0.10.0"
 TERMUX_PKG_SRCURL=git+https://github.com/charmbracelet/vhs
 TERMUX_PKG_GIT_BRANCH="v${TERMUX_PKG_VERSION}"
-TERMUX_PKG_DEPENDS="ttyd"
+TERMUX_PKG_DEPENDS="ttyd, ffmpeg"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 
@@ -15,8 +15,8 @@ termux_step_configure(){
 
 termux_step_make(){
 	sed -i "s|path, _ := launcher.LookPath()|path := \"${TERMUX_PREFIX}/bin/chromium-browser\"|" "$TERMUX_PKG_SRCDIR/vhs.go"
-	go build -o vhs -ldflags "-compressdwarf=false \
-	-X main.Version=${TERMUX_PKG_VERSION} \
+	go build -o vhs -ldflags "-s -w \
+	-X main.Version=v${TERMUX_PKG_VERSION} \
 	-X main.CommitSHA=$(git rev-parse HEAD) \
 	-X main.CommitDate=$(git show --no-patch --format=%cd --date=format:%Y-%m-%d)"
 }
